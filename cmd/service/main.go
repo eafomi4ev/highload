3a9182d1ca83357e99aa298c844f74e86bba35f6
Egor Fomichev <eafomichev@avito.ru>
2023-09-25 19:36:13 +0300

    hw1 - evn variables

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/jackc/pgx/v5"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/jackc/pgx/v5"
+
 	"otus_highload/internal/api/rest/login"
 	"otus_highload/internal/api/rest/user_get_by_id"
 	"otus_highload/internal/api/rest/user_register"
@@ -55,12 +58,38 @@ func main() {
 }
 
 func connectToDB(ctx context.Context) *pgx.Conn {
+	const (
+		POSTGRES_HOST        = "POSTGRES_HOST"
+		POSTGRES_PORT        = "POSTGRES_PORT"
+		POSTGRES_USER_NAME   = "POSTGRES_USER_NAME"
+		POSTGRES_DB_PASSWORD = "POSTGRES_DB_PASSWORD"
+		POSTGRES_DB_NAME     = "POSTGRES_DB_NAME"
+	)
+
+	pgHost := shouldBePresent(POSTGRES_HOST)(os.LookupEnv(POSTGRES_HOST))
+	pgPort := shouldBePresent(POSTGRES_PORT)(os.LookupEnv(POSTGRES_PORT))
+	pgUserName := shouldBePresent(POSTGRES_USER_NAME)(os.LookupEnv(POSTGRES_USER_NAME))
+	pgDBPassword := os.Getenv(POSTGRES_DB_PASSWORD) // для локальной разработки пароля нет; для прода, конечно, должен быть пароль.
+	pgDBName := shouldBePresent(POSTGRES_DB_NAME)(os.LookupEnv(POSTGRES_DB_NAME))
+
+	connString := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", pgUserName, pgDBPassword, pgHost, pgPort, pgDBName)
 	// urlExample := "postgres://username:password@localhost:5432/database_name"
-	conn, err := pgx.Connect(context.Background(), `postgres://localhost:5432/social_db`) // todo: унести в env переменные
+	conn, err := pgx.Connect(context.Background(), connString)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
 
 	return conn
+}
+
+func shouldBePresent(envName string) func(string, bool) string {
+	return func(env string, isPresent bool) string {
+		if isPresent && env != "" {
+			return env
+		}
+
+		log.Fatal(fmt.Sprintf("env %s is not present", envName))
+		return ""
+	}
 }
